d27a930f2521c9c96c789a7850706fc5464a4d4d
Raaj Giri <raj.ash2493@gmail.com>
2026-01-30 23:32:37 +0530

    RestAPI

--- Go_RestAPI/Todo_Auth/internal/config/config.go
+++ Go_RestAPI/Todo_Auth/internal/config/config.go
@@ -1,2 +1,28 @@
 package config
 
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+    PORT string
+    DATABASEURL string
+}
+
+func Load()(*Config, error){
+    var err error = godotenv.Load()
+
+    if err != nil {
+        log.Println("Error while loading env file", err)
+        return nil, err
+    }
+
+    var Config *Config = &Config{
+        PORT: os.Getenv("PORT"),
+        DATABASEURL: os.Getenv("DATABASE_URL"),
+    }
+
+	return Config, nil
+}
