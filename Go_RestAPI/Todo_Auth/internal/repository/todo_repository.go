5a87d131dcab578f0d1d92f4b9ac06463322d9c5
Raaj Giri <raj.ash2493@gmail.com>
2026-02-02 21:13:46 +0530

    RestAPI

--- Go_RestAPI/Todo_Auth/internal/repository/todo_repository.go
+++ Go_RestAPI/Todo_Auth/internal/repository/todo_repository.go
@@ -2,11 +2,23 @@ package repository
 
 import (
 	"Todo_Auth/internal/models"
+	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func CreateTodo(pool *pgxpool.Pool, title string, completed bool) (*models.Todo, error){
 
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx , cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var Query string = `
 	
+	        INSERT INTO Todo
+	 
+
+	`
 }
