7cadeeb772164cba777d055b05e6027d8319124d
Raaj Giri <raj.ash2493@gmail.com>
2026-02-03 21:06:53 +0530

    RestAPI

--- Go_Projects/MoviesAPI/main.go
+++ Go_Projects/MoviesAPI/main.go
@@ -1,17 +1,27 @@
 package moviesapi
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Movies struct {
-	ID int `json:id`;
-	Title string `json:title`;
-	Year int `json:year`;
-	Rating float64 `json:rating`;
-    Director *Director `json:director`;
+	ID       int       `json:"id"`
+	Title    string    `json:"title"`
+	Year     int       `json:"year"`
+	Rating   float64   `json:"rating"`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
-	Name string `json:name`;
-}	
+	Name string `json:"name"`
+}
 
-func main (){
+var movies[] Movies
 
+func main() {
+    router := gin.Default()
+
+	
 }
