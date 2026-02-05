5138e63ba082a9c2fb86243d7dd58e07e84ff6c0
Raaj Giri <raj.ash2493@gmail.com>
2026-02-05 22:25:13 +0530

    Bookstore

--- Go_Projects/BookStore/pkg/routes/book_routes.go
+++ Go_Projects/BookStore/pkg/routes/book_routes.go
@@ -18,5 +18,11 @@ func ConnectDB(){
 	ctx , cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	pool , err := pgxpool.New(ctx,dbURL)
+	if err != nil  {
+		log.Fatal("Failed to connect to DB:", err)
+	}
 	
+
+
 }
