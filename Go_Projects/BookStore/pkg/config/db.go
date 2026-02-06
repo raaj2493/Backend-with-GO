a1df5bac564a2cf99b67d0236f1a48c6fb590319
Raaj Giri <raj.ash2493@gmail.com>
2026-02-06 18:36:28 +0530

    Bookstore

--- Go_Projects/BookStore/pkg/config/db.go
+++ Go_Projects/BookStore/pkg/config/db.go
@@ -1,2 +1,32 @@
 package config
 
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/tools/go/analysis/passes/defers"
+)
+
+var DB *pgxpool.Pool
+
+func ConnectDB(){
+	dbURL := "postgres://postgres:password@localhost:5432/book_db"
+
+	ctx , cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool , err := pgxpool.New(ctx,dbURL)
+	if err != nil  {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatal("DB not reachable:", err)
+	}
+
+	DB = pool
+	log.Println("PostgreSQL connected ✅")
+}
