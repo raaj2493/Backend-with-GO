9304a3ed2e10b141eab5f76a8a6555794e88b02d
Raaj Giri <raj.ash2493@gmail.com>
2026-02-07 18:11:49 +0530

    Bookstore

--- Go_Projects/BookStore/pkg/model/books.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Books struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Author string `json:"Author"`
-	Price int `json:"Price"`
-	CreatedAt string `json:"created_at"`
-}
