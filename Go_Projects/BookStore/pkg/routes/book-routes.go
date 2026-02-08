73c68f1724962982ce7c2c91519fe5096b4fc16d
Raaj Giri <raj.ash2493@gmail.com>
2026-02-08 13:11:32 +0530

    Bookstore

--- Go_Projects/BookStore/pkg/routes/book-routes.go
+++ Go_Projects/BookStore/pkg/routes/book-routes.go
@@ -1,3 +1,15 @@
 package routes
 
+import (
+	"github.com/gin-gonic/gin"
+	"Go_Projects/BookStore/pkg/controllers"
+)
 
+func BookStoreRoutes(router *gin.Engine) {
+
+	router.POST("/book", controllers.CreateBook)
+	router.GET("/book", controllers.GetBook)
+	router.GET("/book/:bookId", controllers.GetBookById)
+	router.PUT("/book/:bookId", controllers.UpdateBook)
+	router.DELETE("/book/:bookId", controllers.DeleteBook)
+}
