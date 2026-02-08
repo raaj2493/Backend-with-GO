db72b4ac6e025a9e6edc2f71087d7c5fa512d9b1
Raaj Giri <raj.ash2493@gmail.com>
2026-02-08 13:22:12 +0530

    Bookstore

--- Go_Projects/BookStore/pkg/config/app.go
+++ Go_Projects/BookStore/pkg/config/app.go
@@ -2,7 +2,7 @@ package app
 
 import(
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 
 )
 
@@ -10,3 +10,14 @@ var (
 	DB *gorm.DB
 )
 
+func Connect(){
+	database , error := gorm.Open("mysql" , "raajgiri:raajgiri@/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if error != nil {
+		panic(error)
+	}
+	DB = database
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
